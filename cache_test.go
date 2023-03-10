bbff661772646154edebbe13b1dc8f00af9c0e20
Oleksandr Kharchenko <okharch@gmail.com>
2023-03-10 11:32:27 +0200

    TestLocal: added unit test which tests whether refresh of value is launched in background

--- cache_test.go
+++ cache_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func init2Caches(t *testing.T, ctx context.Context) (sc1, sc2 *CachedCalculations) {
+func Init2Caches(t *testing.T, ctx context.Context) (sc1, sc2 *CachedCalculations) {
 	externalCache, err := NewRedisCache(ctx)
 	if err != nil {
 		t.Skipf("skip test due external cache not available: %s", err)
@@ -20,6 +20,13 @@ func init2Caches(t *testing.T, ctx context.Context) (sc1, sc2 *CachedCalculation
 	require.NotNil(t, sc1)
 	require.NotNil(t, sc2)
 	return
+}
+
+func TestRemote(t *testing.T) {
+	ctx := context.TODO()
+	cc1, cc2 := Init2Caches(t, ctx)
+	_ = cc1
+	_ = cc2
 }
 
 func TestLocalSimple(t *testing.T) {
@@ -45,4 +52,43 @@ func TestLocalSimple(t *testing.T) {
 	require.Equal(t, 1, d1)
 	require.Equal(t, 1, d2)
 	require.Equal(t, 1, d3)
+}
+
+func TestLocal(t *testing.T) {
+	ctx := context.TODO()
+	counter := 0
+	const p = time.Millisecond * 30
+	const refresh = p * 2
+	const expire = p * 3
+	ccS := NewCachedCalculations(3, nil)
+	require.NotNil(t, ccS)
+	var d1, d2, d3 int
+	var wg sync.WaitGroup
+	getI := func(ctx context.Context) (int, error) { time.Sleep(p); counter++; return counter, nil }
+	GetI := func(result *int) {
+		defer wg.Done()
+		r, err := GetCachedCalc(ccS, ctx, "key", refresh, expire, true, getI)
+		require.NoError(t, err)
+		*result = r
+	}
+	// simple test cache
+	wg.Add(3)
+	GetI(&d1)
+	GetI(&d2)
+	GetI(&d3)
+	require.Equal(t, 1, d1)
+	require.Equal(t, 1, d2)
+	require.Equal(t, 1, d3)
+	wg.Wait()
+	// wait value need to be refreshed and d1 should be the same, but d2 different
+	time.Sleep(refresh + time.Millisecond)
+	wg.Add(1)
+	go GetI(&d1) // should trigger refresh
+	wg.Wait()
+	require.Equal(t, 1, d1)
+	time.Sleep(p + time.Millisecond) // which in p should increase counter
+	require.Equal(t, 2, counter)
+	//require.Equal(t, 1, d1)
+	//require.Equal(t, 1, d2)
+	//require.Equal(t, 1, d3)
 }
