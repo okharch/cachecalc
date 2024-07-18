29772b145c0d3522568105e7e6bb5ef1bfbf8257
Oleksandr Kharchenko <okharch@gmail.com>
2024-07-18 15:53:14 +0300

    fixed TestPostgresCache unit test, used postgres_cache_key_value_expired_v_1_4 table for caching

--- postgrescache_test.go
+++ postgrescache_test.go
@@ -29,8 +29,8 @@ func TestPostgresCache(t *testing.T) {
 	cache, err := NewPostgresCache(ctx, dbURL)
 	require.NoError(t, err, "create cache")
 	require.NotNil(t, cache)
-	_, err = cache.(*PostgresCache).db.Exec("truncate cachecalc")
-	require.NoError(t, err, "truncate cache")
+	_, err = cache.(*PostgresCache).db.Exec("truncate postgres_cache_key_value_expired_v_1_4")
+	require.NoError(t, err, "truncate postgres_cache_key_value_expired_v_1_4")
 
 	defer func() {
 		if err := cache.(*PostgresCache).db.Close(); err != nil {
