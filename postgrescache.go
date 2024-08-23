60b14b29f2b8356c71fb1ab7cff6de4d478eb9c1
Oleksandr Kharchenko <okharch@gmail.com>
2024-08-23 10:43:00 +0300

    SetNX: output hex of value for convenience

--- postgrescache.go
+++ postgrescache.go
@@ -138,7 +138,7 @@ func (p *PostgresCache) SetNX(ctx context.Context, key string, value []byte, ttl
 		return false, err
 	}
 	thread := getThread(ctx)
-	logger.Printf("thread %v: %s=%s expires in %dms", thread, key, string(value), fixTTL.Milliseconds())
+	logger.Printf("thread %v: %s=%x expires in %dms", thread, key, value, fixTTL.Milliseconds())
 	return true, nil
 }
 
